d7bc4fd19ab6d6184bf62c74507c71faa89fd4a0
Admin <admin@unitaskai.com>
2025-12-30 17:13:39 +0700

    feat(layout): wrap AppLayout with ChatProvider
    Summary of changes:
    - Integrated ChatProvider into src/app/(app)/layout.tsx
    - Why: To enable all sub-components within the (app) route group to access shared chat and session state.

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Sidebar from "@/components/Sidebar";
+import { ChatProvider } from "@/context/ChatContext";
 
 export default function AppLayout({
   children,
@@ -8,11 +9,13 @@ export default function AppLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-[100dvh] w-full overflow-hidden bg-white">
-      <Sidebar />
-      <div className="flex-1 flex flex-col min-w-0 h-full relative">
-        {children}
+    <ChatProvider>
+      <div className="flex h-[100dvh] w-full overflow-hidden bg-white">
+        <Sidebar />
+        <div className="flex-1 flex flex-col min-w-0 h-full relative">
+          {children}
+        </div>
       </div>
-    </div>
+    </ChatProvider>
   );
 }
